2b6581456cd2ca0696197d39b4416b4cb3f0223b
AlejandroDel <alejandrohdzd@proton.me>
2025-08-25 12:27:56 -0600

    Update fizzbuzz.js
    Ejecución de "FizzBuzz" en JavaScript

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,14 @@
 export function fizzbuzz(){
-  //tu código va aqui
-  
+// Crear un numero del 1 al 100 en JavaScript
+//"var" se reemplazo por "let" por temas de modernindad
+for (let n1 = 1; n1 <= 100; n1++){
+    console.log (n1)
+// A partir de aqui, se utiliza "if" y "%" (O tambien conocido como Modulación)
+    if(n1 % 3 === 0 && n1 % 5 === 0) {
+        console.log (n1 + " FizzBuzz");
+    } else if (n1 % 5 === 0) {
+        console.log (n1 + " Fizz");
+    } else if (n1 % 3 === 0) {
+        console.log (n1 + " Buzz");
+    }
 }
